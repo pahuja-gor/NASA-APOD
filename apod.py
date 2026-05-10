738917041d21dcda98af9e4c655dda414bfe6c51
Goral <36912136+pahuja-gor@users.noreply.github.com>
2026-05-10 01:10:06 -0700

    Update README generation for image availability

--- apod.py
+++ apod.py
@@ -6,27 +6,50 @@ url = 'https://apod.nasa.gov/apod/astropix.html'
 get_request = requests.get(url)
 soup = BeautifulSoup(get_request.content, 'html.parser')
 
-pic_of_the_day = base + soup.find('img').parent['href']
-caption = soup.findAll('center')[1].find('b').text.strip()
+img_element = soup.find('img')
 
-README_TEMPLATE = f'''
-<div align="center">
-  <h1>
-    NASA-APOD
-  </h1>
-</div>
+if img_element:
+  pic_of_the_day = base + img_element.parent['href']
+  caption = soup.findAll('center')[1].find('b').text.strip()
   
-<div align="center">
-  A fun program that displays NASA's Astronomy Picture of the Day
-</div>
-
-<br>
-
-![]({pic_of_the_day})
-
-<p align = "center">
-  <b>{caption}</b>
-</p>
-'''
-
-open('README.md', 'w').writelines(README_TEMPLATE)
+  README_TEMPLATE = f'''
+  <div align="center">
+    <h1>
+      NASA-APOD
+    </h1>
+  </div>
+    
+  <div align="center">
+    A fun program that displays NASA's Astronomy Picture of the Day
+  </div>
+  
+  <br>
+  
+  ![]({pic_of_the_day})
+  
+  <p align = "center">
+    <b>{caption}</b>
+  </p>
+  '''
+  
+  open('README.md', 'w').writelines(README_TEMPLATE)
+else:
+  README_TEMPLATE = f'''
+  <div align="center">
+    <h1>
+      NASA-APOD
+    </h1>
+  </div>
+    
+  <div align="center">
+    A fun program that displays NASA's Astronomy Picture of the Day
+  </div>
+  
+  <br>
+  
+  <p align = "center">
+    <b>NO IMAGE FOR TODAY :)</b>
+  </p>
+  '''
+  
+  open('README.md', 'w').writelines(README_TEMPLATE)
